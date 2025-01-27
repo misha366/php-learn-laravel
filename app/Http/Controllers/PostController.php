91a7119dcb956f44043f7fb741010443e587b20b
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-27 21:57:27 +0300

    refactored PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,14 +12,24 @@ class PostController extends Controller
 
     public function getFirstPost() : string {
         $post = Post::find(self::FIRST_POST_INDEX);
+        return $this->htmlPost($post);
+    }
+
+    public function getAllPosts() : string
+    {
+        $result = "";
+        foreach (Post::all() as $post) {
+            $result .= $this->htmlPost($post);
+        }
+        return $result;
+    }
+
+    // Accepts a Post object and returns it in html form
+    private function htmlPost(Post $post) : string {
         return "
             <h1>#$post->id $post->title</h1>
             <div>$post->content</div>
+            <hr>
        ";
-    }
-
-    public function getAllPosts() : Collection
-    {
-        return Post::all();
     }
 }
