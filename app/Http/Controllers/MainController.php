559ab7ac4b5cc00216a08d8d87faf3e264413ba2
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-29 06:13:05 +0300

    refactored MainController

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,7 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\View\View;
 
 class MainController extends Controller {
+    const VIEW_NAME_INDEX = "index";
+
+    const VIEW_DATA_KEY_TITLE = "title";
+    const VIEW_DATA_KEY_TITLE_VALUE = "Homepage";
+
     public function index() : View {
-        return view('index', ['title' => "Homepage"]);
+        return view(self::VIEW_NAME_INDEX, [
+            self::VIEW_DATA_KEY_TITLE => self::VIEW_DATA_KEY_TITLE_VALUE
+        ]);
     }
 }
