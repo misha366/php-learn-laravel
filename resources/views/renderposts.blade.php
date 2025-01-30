6f6685b75b9e9da31818a143ab044832d9818017
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-30 03:53:03 +0300

    added edit/delete buttons

--- resources/views/renderposts.blade.php
+++ resources/views/renderposts.blade.php
@@ -10,9 +10,15 @@
     <div>{{ $post->content }}</div>
 @else
     @foreach($posts as $post)
-        <h3>
+        <h3 class="post__title">
             <a href="{{ route("post.getPost", ["id" => $post->id]) }}">
                 #{{ $post->id }} {{ $post->title }}
+            </a>
+            <a href="{{ route("post.updatePostView", ["id" => $post->id]) }}" class="post__link-edit">
+                <img src="https://cdn-icons-png.flaticon.com/512/1159/1159633.png" alt="edit">
+            </a>
+            <a href="#" class="post__link-trash">
+                <img src="https://cdn-icons-png.flaticon.com/512/5258/5258411.png" alt="edit">
             </a>
         </h3>
         <div>{{ $post->content }}</div>
