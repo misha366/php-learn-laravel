43c60b3f81f0271f060006647509628097b4edff
misha366 <misha.moskalenko.jv@gmail.com>
2025-02-01 11:14:54 +0300

    created new model (category) & added fk (many (post) to one (category))

---
+++ database/migrations/2025_02_01_075131_add_category_id_to_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Добавление связи One to Many (category 1 posts n)
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->index('category_id', 'post_category_idx');
+            $table->foreign('category_id', 'post_category_fk')
+                ->on('categories')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            Schema::table('posts', function (Blueprint $table) {
+                $table->dropForeign('post_category_fk');
+                $table->dropIndex('post_category_idx');
+                $table->dropColumn('category_id');
+            });
+        });
+    }
+};
