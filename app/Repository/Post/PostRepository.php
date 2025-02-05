f664736075919416c944383e5bb3e411dfc0a368
misha366 <misha.moskalenko.jv@gmail.com>
2025-02-05 05:37:53 +0300

    added transactions to the repository

--- app/Repository/Post/PostRepository.php
+++ app/Repository/Post/PostRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository\Post;
 use App\DTO\PostDTO;
 use App\Models\Post;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\DB;
 
 class PostRepository implements PostRepositoryInterface
 {
@@ -25,25 +26,32 @@ class PostRepository implements PostRepositoryInterface
 
     public function store(PostDTO $postDTO): Post
     {
-        $post = Post::create(PostDTO::toArray($postDTO));
+        // use для того чтобы использовать переменные, декларируемые за пределами ф-и
+        return DB::transaction(function () use ($postDTO) {
+            $post = Post::create(PostDTO::toArray($postDTO));
 
-        if (!empty($postDTO->getTagIds())) {
-            $post->tags()->sync($postDTO->getTagIds());
-        }
+            if (!empty($postDTO->getTagIds())) {
+                $post->tags()->sync($postDTO->getTagIds());
+            }
 
-        return $post;
+            return $post;
+        });
     }
 
     public function update(PostDTO $postDTO, Post $post): Post
     {
-        $post->update(PostDTO::toArray($postDTO));
+        return DB::transaction(function () use ($post, $postDTO) {
+            $post->update(PostDTO::toArray($postDTO));
 
-        $post->tags()->sync($postDTO->getTagIds() ?? []);
-        return $post;
+            $post->tags()->sync($postDTO->getTagIds() ?? []);
+            return $post;
+        });
     }
 
     public function destroy(Post $post): void
     {
-        $post->delete();
+        DB::transaction(function () use ($post) {
+            $post->delete();
+        });
     }
 }
