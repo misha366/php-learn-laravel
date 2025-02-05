80e8e449579e032055dd3d4997e01f34e137f34c
misha366 <misha.moskalenko.jv@gmail.com>
2025-02-05 12:06:08 +0300

    created first admin page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\PostService;
 use Illuminate\View\View;
 
 class AdminController
 {
+    private PostService $postService;
+
+    public function __construct(PostService $postService) {
+        $this->postService = $postService;
+    }
+
     public function index(): View
     {
-        return view("admin.index");
+        $posts = $this->postService->getPaginatedAndFilteredPosts(null, null);
+        return view("admin.index", [
+            "posts" => $posts,
+        ]);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,7 +7,28 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="container-fluid">
+        @foreach($posts as $post)
+            <div class="card mb-3">
+                <div class="card-body d-flex justify-content-between">
+                    <div class="flex-grow-1 d-flex flex-column">
+                        <h3 class="display-5 flex-grow-1">
+                            <a href="">#1 - {{ $post->title }}</a>
+                        </h3>
+                        <p class="card-text text-muted small">
+                            <a href="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, minima!</a>
+                        </p>
+                    </div>
+                    <div class="d-flex flex-column align-items-end">
+                        <a class="btn btn-primary btn-sm mb-2" href="#">Edit</a>
+                        <form method="POST">
+                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @stop
 
 @section('css')
