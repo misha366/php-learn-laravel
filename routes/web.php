891d0751e31262b8376dd32029f54c121aee6a08
misha366 <misha.moskalenko.jv@gmail.com>
2025-02-11 20:15:33 +0300

    refactored routes

--- routes/web.php
+++ routes/web.php
@@ -21,22 +21,25 @@ Route::get("/", [MainController::class, "index"])->name("main.home");
 Route::group(["namespace" => ""], function () {
     Route::get("/about", IndexController::class)->name("about.index");
 });
+
 Route::get("/admin", [AdminController::class, "index"])->name("admin.index");
 
-// CREATE
-Route::get("/posts/create", [PostController::class, "create"])->name("posts.create");
-Route::post("/posts", [PostController::class, "store"])->name("posts.store");
+Route::group(['namespace' => '/posts'], function () {
+    // CREATE
+    Route::get("/posts/create", [PostController::class, "create"])->name("posts.create");
+    Route::post("/posts", [PostController::class, "store"])->name("posts.store");
 
-// READ
-Route::get("/posts", [PostController::class, "index"])->name("posts.index");
-Route::get("/posts/{post}", [PostController::class, "show"])->name("posts.show");
+    // READ
+    Route::get("/posts", [PostController::class, "index"])->name("posts.index");
+    Route::get("/posts/{post}", [PostController::class, "show"])->name("posts.show");
 
-// UPDATE
-Route::get("/posts/{post}/edit", [PostController::class, "edit"])->name("posts.edit");
-Route::patch("/posts/{post}", [PostController::class, "update"])->name("posts.update");
+    // UPDATE
+    Route::get("/posts/{post}/edit", [PostController::class, "edit"])->name("posts.edit");
+    Route::patch("/posts/{post}", [PostController::class, "update"])->name("posts.update");
 
-// DELETE
-Route::delete("/posts/{post}", [PostController::class, "destroy"])->name("posts.destroy");
+    // DELETE
+    Route::delete("/posts/{post}", [PostController::class, "destroy"])->name("posts.destroy");
+});
 
 Route::middleware([
     'auth:sanctum',
